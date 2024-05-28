60fe9368a6a00a17d88cd428429c21fa9ca60ffc
JackelineSalome <89036070+JackelineSalome@users.noreply.github.com>
2024-05-28 17:25:46 -0500

    feat: add upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { SearchComponent } from './search/search.component';
+import { UploadComponent } from './upload/upload.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path:'search', component: SearchComponent}
+    { path:'search', component: SearchComponent},
+    { path:'upload', component: UploadComponent}
 ];
